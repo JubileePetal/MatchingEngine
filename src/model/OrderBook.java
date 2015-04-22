bd2585e4a398e07c4256b095dffadc5ba530021e
Circus <circus@lindseytop.(none)>
2015-04-22 16:25:50 +0200

    added functionality to add an order in the orderbook

--- src/model/OrderBook.java
+++ src/model/OrderBook.java
@@ -1,9 +1,50 @@
 package model;
 
+import java.util.HashMap;
+
+import models.LimitOrder;
+import models.OpCodes;
+import models.Order;
+
 public class OrderBook  {
 
+	private HashMap<Double,LimitOrder> buyOrders;
+	private HashMap<Double,LimitOrder> sellOrders;
+	
 	public OrderBook() {
-		// TODO Auto-generated constructor stub
+		buyOrders = new HashMap<Double,LimitOrder>();
+		sellOrders = new HashMap<Double,LimitOrder>();
 	}
+	
+	public void enterOrder(Order order) {
+		
+		if(order instanceof LimitOrder) {
+			
+			LimitOrder limitOrder = (LimitOrder) order;
+			
+			int orderType = order.getOrderType();
+			
+			if(orderType == OpCodes.BUY_ORDER) {
+				addToHash(buyOrders, limitOrder);
+			} else if(orderType == OpCodes.SELL_ORDER) {
+				addToHash(sellOrders, limitOrder);
+			}
+			System.out.println("This is a Limit Order");
+			
+		} else {
+			System.out.println("This is a Market Order");
+			//TODO handle market order			
+		}
+	}
+	
+	private void addToHash(HashMap<Double, LimitOrder> map , LimitOrder order) {
+
+		Double price = order.getPrice();
+		
+		synchronized(map) {
+			map.put(price, order);
+		}
+	}
+	
 
 }
