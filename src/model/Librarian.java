6d0d5862db5c87a5b9d44e341742e28e401b063b
tinyeyra <anna@Annas-MacBook-Pro-2.local>
2015-04-24 10:58:58 +0200

    update

--- src/model/Librarian.java
+++ src/model/Librarian.java
@@ -62,6 +62,7 @@ public class Librarian implements Observer {
 			String key = order.getInstrument().getAbbreviation();
 			
 			OrderBook ob = library.get(key);
+			ob.enterOrder(order);
 			
 			
 		
