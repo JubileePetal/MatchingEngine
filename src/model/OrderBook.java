2a23195873b4282884ced7f05fc89013d322a693
Circus <circus@lindseytop.(none)>
2015-04-24 11:32:45 +0200

    orderbook now successfully distinguishes between market and limit order

--- src/model/OrderBook.java
+++ src/model/OrderBook.java
@@ -2,42 +2,43 @@ package model;
 
 import java.util.HashMap;
 
-import models.LimitOrder;
 import models.OpCodes;
 import models.Order;
 
 public class OrderBook  {
 
-	private HashMap<Double,LimitOrder> buyOrders;
-	private HashMap<Double,LimitOrder> sellOrders;
+	private HashMap<Double,Order> buyOrders;
+	private HashMap<Double,Order> sellOrders;
 	
 	public OrderBook() {
-		buyOrders = new HashMap<Double,LimitOrder>();
-		sellOrders = new HashMap<Double,LimitOrder>();
+		buyOrders = new HashMap<Double,Order>();
+		sellOrders = new HashMap<Double,Order>();
 	}
 	
 	public void enterOrder(Order order) {
 		
-		if(order instanceof LimitOrder) {
+		int typeOfOrder = order.getTypeOfOrder();
+		
+		if(typeOfOrder == OpCodes.LIMIT_ORDER) {
 			
-			LimitOrder limitOrder = (LimitOrder) order;
-			
-			int orderType = order.getOrderType();
+			int orderType = order.isBuyOrSell();
 			
 			if(orderType == OpCodes.BUY_ORDER) {
-				addToHash(buyOrders, limitOrder);
+				addToHash(buyOrders, order);
 			} else if(orderType == OpCodes.SELL_ORDER) {
-				addToHash(sellOrders, limitOrder);
+				addToHash(sellOrders, order);
 			}
+			
 			System.out.println("This is a Limit Order");
 			
-		} else {
+		} else if(typeOfOrder == OpCodes.MARKET_ORDER) {
+			
 			System.out.println("This is a Market Order");
 			//TODO handle market order			
 		}
 	}
 	
-	private void addToHash(HashMap<Double, LimitOrder> map , LimitOrder order) {
+	private void addToHash(HashMap<Double, Order> map , Order order) {
 
 		Double price = order.getPrice();
 		
