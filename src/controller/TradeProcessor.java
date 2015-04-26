fbafdf2f11f31e1f580053fd340f7c4a576a0959
tinyeyra <anna@client236-215.wireless.umu.se>
2015-04-26 13:36:53 +0200

    Update

--- src/controller/TradeProcessor.java
+++ src/controller/TradeProcessor.java
@@ -15,15 +15,37 @@ public class TradeProcessor {
 		Trade trade = new Trade();
 		if(order1.isBuyOrSell() == OpCodes.BUY_ORDER){
 			
-			trade.setBuyer("HAMMOCK");
+			setBuyerInfo(order1, trade);
+			setSellerInfo(order2, trade);
 			
 		}else{
 			
+			setBuyerInfo(order2, trade);
+			setSellerInfo(order1, trade);
+			
 		}
 		
+		//trade.setQuantity();
+		
+
 		
 		
+		
+ 	}
+	
+	private void setSellerInfo(Order sellOrder, Trade trade){
+		
+		trade.setSeller(sellOrder.getMyOwner());
+		trade.setSelOrderID(sellOrder.getId());
 	}
+	
+	private void setBuyerInfo(Order buyOrder, Trade trade){
+		
+		trade.setBuyer(buyOrder.getMyOwner());
+		trade.setBuyOrderID(buyOrder.getId());
+	}
+	
+	
 }
 
 
