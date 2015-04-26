281d63b518d26a71479b884008c09f50dca0e522
tinyeyra <anna@client236-215.wireless.umu.se>
2015-04-26 13:19:44 +0200

    added get orderbook in librarian and worker

--- src/controller/TradeProcessor.java
+++ src/controller/TradeProcessor.java
@@ -1,9 +1,29 @@
 package controller;
 
+import models.OpCodes;
+import models.Order;
+
 public class TradeProcessor {
 
 	public TradeProcessor() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	
+	public void createTrade(Order order1, Order order2){
+		
+		Trade trade = new Trade();
+		if(order1.isBuyOrSell() == OpCodes.BUY_ORDER){
+			
+			trade.setBuyer("HAMMOCK");
+			
+		}else{
+			
+		}
+		
+		
+		
+	}
 }
+
+
